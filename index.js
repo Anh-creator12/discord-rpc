fca8e1a329d71e1f28abe36aa5ce23d1cde23840
Anh <ssungjihoon@gmail.com>
2026-09-06 21:36:27 +0700

    Update Discord.js version and modify bot presence

--- index.js
+++ index.js
@@ -1,38 +1,20 @@
-const { Client, RichPresence } = require('discord.js-selfbot-v13');
+const Discord = require('discord.js-selfbot-v11');
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 3000;
+app.get('/', (req, res) => res.send('Bot is running!'));
+app.listen(process.env.PORT || 3000);
 
-// Web server giữ cho ứng dụng hoạt động trên cloud
-app.get('/', (req, res) => {
-  res.send('Discord RPC đang hoạt động 24/7!');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setPresence({
+    game: {
+      name: 'Minecraft',
+      type: 'PLAYING',
+    }
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Server đang lắng nghe tại port ${port}`);
-});
-
-const client = new Client();
-
-client.on('ready', async () => {
-  console.log(`Đã đăng nhập thành công vào tài khoản: ${client.user.tag}`);
-
-  // Thiết lập Rich Presence
-  const getPresence = () => {
-    return new RichPresence(client)
-      .setApplicationId('154599907290370054')
-      .setType('PLAYING')
-      .setName('bender')
-      .setDetails('bender')
-      .setState('bender')
-      .setStartTimestamp(Date.now())
-      .setAssetsLargeImage('cover')
-      .setAssetsLargeText('bender');
-  };
-
-  client.user.setPresence({ activities: [getPresence()] });
-});
-
-// Đăng nhập an toàn qua biến môi trường
 client.login(process.env.DISCORD_TOKEN);
